c32af5618b1f7f8977a9c945ca59c7e2cda4af7c
Daniel Pett <dejp3@cam.ac.uk>
2019-05-03 10:07:54 +0100

    Added current files to repo

---
+++ footer.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package FitzIntranet
+ */
+
+?>
+
+	</div><!-- #content -->
+	<div class="bg-white p-2">
+	    <div class="row mx-auto">
+	        <div class="mx-auto mt-3 " id="sponsors">
+	            <a href="http://www.museums.cam.ac.uk"><img class="img-responsive" alt="University of Cambridge Museums logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/ucm_logo_black_white.png"></a>
+	            <a href="http://www.artscouncil.org.uk/what-we-do/raising-standards/designation-scheme/"><img class="img-responsive" alt="Designation scheme Logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/designation.png"></a>
+	            <a href="http://www.artscouncil.org.uk/what-we-do/supporting-museums/accreditation-scheme/"><img class="img-responsive" alt="Accredited Museum status logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/accredited.png"></a>
+	            <a href="http://www.cambridge.gov.uk/"><img class="img-responsive" alt="Cambridge City Council Logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/camcity_logo.png"></a>
+	            <a href="http://www.artscouncil.org.uk/"><img class="img-responsive" alt="Arts Council England Logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/ace.png"></a>
+	            <a href="https://re.ukri.org/"><img class="img-responsive" alt="Research England logo" src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/research_england.png"></a>
+
+	        </div>
+	    </div>
+	</div>
+	<footer class="text-white">
+
+	    <div class="container ">
+	        <div class="row">
+	            <div class="col-sm mt-2 mb-2 border-right border-white">
+	                <h4>Contact us</h4>
+	                <p>
+	                    The Fitzwilliam Museum,<br/>
+	                    Trumpington Street,<br/>
+	                    Cambridge<br/>
+	                    CB2 1RB<br/>
+	                    Tel: +44 (0)1223 332900
+	                </p>
+	            </div>
+	            <div class="col-sm mt-2 mb-2 border-right border-white ">
+	                <h4>Useful links</h4>
+
+	                <ul class="share">
+	                    <li><a href="https://www.museums.cam.ac.uk/">University of Cambridge Museums</a></li>
+	                    <li><a href="https://www.fitzmuseum.cam.ac.uk/website/tou">Website Terms of Use</a></li>
+	                    <li><a href="https://www.fitzmuseum.cam.ac.uk/website/cookiepolicy">Privacy and cookies</a></li>
+	                    <li><a href="https://www.fitzmuseum.cam.ac.uk/website/cookiepolicy">Accessibility</a></li>
+	                </ul>
+	            </div>
+	            <div class="col-sm mt-2 mb-2 border-dotted border-right border-white ">
+	                <h4>Fitz Social</h4>
+	                <p class="share">
+	                    <a href="https://twitter.com/FitzMuseum_UK"><i class="fab fa-twitter fa-2x p-2"></i></a>
+	                    <a href="https://www.instagram.com/fitzmuseum_uk"><i class="fab fa-instagram fa-2x  p-2"></i></a>
+	                    <a href="https://www.facebook.com/fitzwilliammuseum/"><i class="fab fa-facebook fa-2x p-2"></i></a>
+	                    <a href="https://www.github.com/fitzwilliammuseum/"><i
+	                            class="fab fa-github fa-2x p-2"></i></a>
+	                    <a href="https://www.youtube.com/channel/UCFwhw5uPJWb4wVEU3Y2nScg"><i
+	                            class="fab fa-youtube fa-2x p-2"></i></a>
+	                </p>
+	            </div>
+	            <div class="col-sm mt-2 mb-2">
+	                <a href="https://www.fitzmuseum.cam.ac.uk"><img src="https://creative-economy.fitzmuseum.cam.ac.uk/images/logos/FitzwilliamLogoFooter.png" class="rounded d-block"/></a>
+	                <p class="share">
+	                    &copy; 2019 The University of Cambridge<br/>
+	                     Code: <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY</a>
+	                </p>
+	            </div>
+	        </div>
+	    </div>
+
+	    <a href="#0" class="cd-top js-cd-top">Top</a>
+
+	</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
