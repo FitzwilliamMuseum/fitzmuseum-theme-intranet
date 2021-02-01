7ccee58870526c13fbbda21becfd0a9af4aa10fb
Daniel Pett <dejp3@cam.ac.uk>
2021-02-01 22:58:01 +0000

    Update scripts

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,18 +3,16 @@
  * Enqueue scripts and styles.
  */
 function fitzmuseum_scripts() {
-	wp_enqueue_style( 'bootstrap-four-cdn', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
-	wp_enqueue_style( 'material-site-style', get_stylesheet_directory_uri() . '/css/material.css');
+	wp_enqueue_style( 'bootstrap-four-cdn', 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css' );
 	wp_enqueue_style( 'fitz-site-style', get_stylesheet_directory_uri() . '/css/site.css');
 
 	wp_enqueue_style( 'fontawesome-style', 'https://use.fontawesome.com/releases/v5.5.0/css/all.css');
   wp_enqueue_style( 'google-fonts-style', 'https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i|Roboto+Mono:300,400,700|Roboto+Slab:300,400,700');
 	wp_enqueue_style( 'material-style-css', 'https://fonts.googleapis.com/icon?family=Material+Icons');
 
-	wp_enqueue_script( 'jquery-cdn', 'https://code.jquery.com/jquery-3.4.1.min.js', $in_footer = true );
-	wp_enqueue_script( 'popper-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', $in_footer = true );
-	wp_enqueue_script( 'bootstrap-js-cdn','https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js', $in_footer = true );
-	wp_enqueue_script( 'material-js', get_template_directory_uri() . '/js/material.js' , $in_footer = true );
+	wp_enqueue_script( 'jquery-cdn', 'https://code.jquery.com/jquery-3.5.1.min.js', $in_footer = true );
+	wp_enqueue_script( 'popper-cdn', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', $in_footer = true );
+	wp_enqueue_script( 'bootstrap-js-cdn','https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js', $in_footer = true );
 	wp_enqueue_script( 'config-js', get_template_directory_uri() . '/js/config.js' , $in_footer = true );
 	wp_enqueue_script( 'klaro-js', get_template_directory_uri() .'/js/klaro.js', $in_footer = true  );
 
